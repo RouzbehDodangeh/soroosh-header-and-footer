991a7d934ac266dc85b505711f297ce678fb678e
RouzbehDodangeh <dodangehrouzbeh1@gmail.com>
2022-09-05 13:53:53 +0430

    last update 1:53 index.html and style.css added

---
+++ js/body.js
@@ -0,0 +1,5 @@
+const topnav=document.querySelector("topnav");
+const myLinks=document.querySelector("myLinks");
+
+myLinks.addEventListener("click", function (e) {
+    topnav.classList.toggle("d-none")})
